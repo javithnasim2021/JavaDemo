ff7f786682482427ecd71dd57a76eb7978dba017
javithnasim2021 <79827965+javithnasim2021@users.noreply.github.com>
2021-06-04 14:13:10 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 class HelloWorld{
 	public static void main(String[] args){
 	System.out.println("Welcome to Tcs");
-	System.out.println("github webhook added try 2");
+	System.out.println("github webhook added and ngrok started");
 }
 }
